a890eda824b24db8af17608df34eb93e9e84acea
dmcatt <declan@aspinity.com>
2024-07-15 10:29:13 -0400

    .

--- AppQuit.c
+++ AppQuit.c
@@ -6,25 +6,31 @@ void SDL_AppQuit(void* appstate) {
     running = 0;
 
 #ifndef USE_WINDOW_SURFACE
-    if (main_texture)
+    if (main_texture) {
         SDL_DestroyTexture(main_texture);
         main_texture = NULL;
+    }
 
-    if (pixels)
+    if (pixels) {
         SDL_free(pixels);
         pixels = NULL;
+    }
+    
 
-    if (renderer)
+    if (renderer) {
         SDL_DestroyRenderer(renderer);
         renderer = NULL;
+    }
+        
 #else
     SDL_UnlockSurface(win_surface);
 #endif
     
 
-    if (window)
+    if (window) {
         SDL_DestroyWindow(window);
         window = NULL;
-
+    }
+        
     SDL_Log("freed shit");
 }
